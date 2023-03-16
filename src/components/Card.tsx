4f0c8341c04d16c43c9bed911e7e128ee51f3721
fikri amanda <fikriamd12@gmail.com>
2023-03-16 06:05:52 -1200

    feat: add endpoint for home page

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,51 +1,48 @@
 import React from "react";
 import { FC } from "react";
 import { useNavigate } from "react-router";
-import image from "../assets/div.svg";
 import { useCookies } from "react-cookie";
 
 interface CardProps {
-  homestay_id?: number
-  name?: string
-  description?: string
-  price?: number
-  gambar?: string
-  images?: string
-
+  homestay_id?: string;
+  name?: string;
+  description?: string;
+  price?: number;
+  gambar?: string;
+  images?: string;
 }
-const Card: FC<CardProps> = (
-  { homestay_id,
-    name,
-    description,
-    price,
-    gambar,
-    images, }
-) => {
-  const navigate = useNavigate();
 
-  function onCLickDetail() {
-    navigate(`/rooms/:id_rooms}`);
-  }
+const Card: FC<CardProps> = ({
+  homestay_id,
+  name,
+  description,
+  price,
+  gambar,
+  images,
+}) => {
+  const navigate = useNavigate();
+  const [cookie, setCookie] = useCookies(["token"]);
+
   return (
-    <div className="card card-compact w-96 bg-transparent p-4">
-      <figure onClick={() => onCLickDetail()}>
+    <div id={homestay_id} className="card card-compact w-96 bg-transparent p-4">
+      <figure>
         <img
           src={images}
-          alt="Nama Tempat"
+          alt={name}
           className="w-64 aspect-auto object-contain"
         />
       </figure>
-      <div className="card-body items-start justify-between m-7 -mt-1" onClick={() => onCLickDetail()}>
-        <h2 className="card-title">{name}</h2>
+      <div
+        className="card-body items-start justify-between m-7 -mt-1"
+      >
+        <p className="card-title">{name}</p>
         <div className="rating space-x-2">
           <input
             type="radio"
             name="rating-4"
             className="mask mask-star-2 bg-[#0E8388]"
           />
-          <p className="text-start text-xl font-semibold -mt-1">
-            4,9
-          </p>
+          <p className="text-start text-xl font-semibold -mt-1">4,9</p>
         </div>
         <div className="card-actions justify-start">
           <p className="text-start text-xl font-semibold">
--- src/pages/HomePage/Home.tsx
+++ src/pages/HomePage/Home.tsx
@@ -1,42 +1,41 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
+import { useCookies } from "react-cookie";
 import axios from "axios";
+
+import Swal from "../../utils/Swal";
 import withReactContent from "sweetalert2-react-content";
 
 import Layout from "../../components/Layout";
+import Navbar from "../../components/Navbar";
 import Card from "../../components/Card";
-import Swal from "../../utils/Swal";
 import Logo from "../../assets/LogoBookaBed.webp";
 import Button from "../../components/Button";
 import Input from "../../components/Input";
+
 import { Link } from "react-router-dom";
 import { IoSearchCircleSharp } from "react-icons/io5";
 import { RiMenuFill, RiUser3Line } from "react-icons/ri";
 
-interface homeTypes {
-  homestay_id: number
-  name: string
-  description: string
-  price: number
-  gambar: string
-  images: string
+interface HomeProps {
+  [x: string]: any;
+  id?: number;
+  name?: string;
+  description?: string;
+  price?: number;
+  gambar?: string;
+  images?: string;
 }
 
-const Home = () => {
-  const [home, setHome] = useState<homeTypes[]>([]);
+const Home: React.FC<HomeProps> = () => {
+  const MySwal = withReactContent(Swal);
+  const [home, setHome] = useState<HomeProps[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const [search, setSearch] = useState<string>("");
   const [numDisplayed, setNumDisplayed] = useState<number>(8);
 
-  function handleSearchChange(event: React.ChangeEvent<HTMLInputElement>) {
-    setSearch(event.target.value);
-  }
-  const filteredHome = home.filter((item) =>
-    item.name.toLowerCase().includes(search.toLowerCase())).slice(0, numDisplayed);
-
-  function loadMore() {
-    setNumDisplayed(numDisplayed + 8);
-  }
+  const [cookie, setCookie] = useCookies(["token"]);
+  const checkToken = cookie.token;
 
   useEffect(() => {
     fetchHome();
@@ -45,97 +44,38 @@ const Home = () => {
   function fetchHome() {
     setLoading(true);
     axios
-      .get(`https://virtserver.swaggerhub.com/AirBnBProject/AirBnB/1.0.0/homestay`)
+      .get(`http://18.142.43.11:8080/homestay`, {
+        headers: {
+          Authorization: `Bearer ${checkToken}`,
+        },
+      })
       .then((res) => {
+        console.log(res.data.data);
         setHome(res.data.data);
-      }).catch((err) => {
+      })
+      .catch((err) => {
         alert(err.response.data.message);
       })
       .finally(() => setLoading(false));
-
   }
 
   return (
     <Layout>
-      <div className="h-1/5 w-full shadow-md pb-6">
-        <div className="flex flex-row items-center justify-between pt-6 pl-24">
-          <div className="flex flex-row items-center gap-8">
-            <Link to={"/"}>
-              <h1 className="text-3xl text-color3 font-bold ">
-                BookaBed
-              </h1>
-            </Link>
-            <img
-              src={Logo}
-              alt="Logo"
-              className="w-12
-            "
-            />
-          </div>
-          <div className="form-control w-96 h-10 bg-color4 rounded-3xl flex-row flex">
-            <Input
-              id="search"
-              type="text"
-              placeholder="Cari tempat menginap..."
-              className="bg-color4 rounded-3xl h-10 pl-10 w-[21rem] "
-              onChange={handleSearchChange}
-            />
-            <IoSearchCircleSharp className="w-7 h-7 text-color3 self-center" />
-          </div>
-          <div
-            className="flex gap-16 pr-32 
-        "
-          >
-            <div className="dropdown dropdown-hover ">
-              <label
-                tabIndex={0}
-                className="btn btn-ghost btn-circle "
-              >
-                <RiMenuFill className="w-7 h-7 text-color3 " />
-              </label>
-              <ul
-                tabIndex={0}
-                className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52 font-semibold"
-              >
-                <li>
-                  <a href="/login">Login</a>
-                </li>
-                <li>
-                  <a href="/register">Register</a>
-                </li>
-                <li>
-                  <a href="/reservasi">Riwayat Reservasi</a>
-                </li>
-              </ul>
-            </div>
-            <div className="pt-2">
-              <Link to={"/profile"}>
-                <RiUser3Line className="w-7 h-7 text-color3 " />
-              </Link>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Navbar />
       <div className="container mx-auto flex flex-col justify-center">
         <div className="my-10 text-black">
           <div className="m-2 grid grid-flow-row auto-rows-max grid-cols-2 lg:grid-cols-4 gap-3">
-            {filteredHome.map((item) => (
+            {home.map((item: any, index) => (
               <Card
-                key={item.homestay_id}
+                key={index}
+                homestay_id={item.homestay_id}
                 name={item.name}
-                description={item.description}
                 price={item.price}
-                gambar={item.gambar}
+                images={item.images}
               />
             ))}
           </div>
         </div>
-        <Button
-          id="btn-loadMore"
-          label="Lihat Lebih Banyak"
-          className="bg-color3 border-2 border-color3 rounded-xl py-3 mx-auto text-white font-poppins font-semibold w-[20%] mt-10 hover:bg-color4 hover:text-white hover:border-none self-center"
-          onClick={loadMore}
-        />
       </div>
     </Layout>
   );
